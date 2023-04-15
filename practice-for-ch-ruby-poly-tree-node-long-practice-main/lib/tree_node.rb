9fe7cef272d5f9fb6ea156c15ae12f34b999889c
Hanna Darwish <hannadarwish@gmail.com>
2023-04-14 17:30:41 -0700

    worked on dfs method for PolyTreeNode.rb

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -33,5 +33,18 @@ class PolyTreeNode
         self.children.delete(selected_child)
         selected_child.parent = nil
     end
+
+    def dfs(target_value)
+        return self if self.value == target_value
+        if self.children.empty?
+            self.parent.children.shift
+            dfs()
+
+
+        first_child = self.children[0]
+        if first_child != target_value
+            self.children.shift.dfs(target_value)
+        end
+    end
     
 end
