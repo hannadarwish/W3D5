e3012402a9c27c5848b4e59e44adab279a8f14e4
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-04-14 17:13:22 -0700

    defined methods for PolyTreeNode project

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -10,13 +10,28 @@ class PolyTreeNode
 
     # assign child to its parent
     # assign parent to the child
-    def parent=(value)
-        unless value == nil
-            @parent = value
-            if !children.include?(self)
-                value.children << self
+    def parent=(new_parent)
+        if self.parent == nil
+            @parent = new_parent
+            new_parent.children << self
+        else
+            #!new_parent.children.include?(self)
+            self.parent.children.delete(self)
+            if new_parent != nil
+                new_parent.children << self
             end
-        end
+            @parent = new_parent
+        end  
     end
-  
+
+    def add_child(new_child)
+        new_child.parent = self
+        self.children << new_child if !self.children.include?(new_child)
+    end
+
+    def remove_child(selected_child)
+        self.children.delete(selected_child)
+        selected_child.parent = nil
+    end
+    
 end
